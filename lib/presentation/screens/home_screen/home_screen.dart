558676905ed935ec00f9ab26fc06c67a5f086779
Ziborov Vadim <vadim.ziborov1985@gmail.com>
2024-07-02 16:10:12 +0500

    Изменил иконку

--- lib/presentation/screens/home_screen/home_screen.dart
+++ lib/presentation/screens/home_screen/home_screen.dart
@@ -5,6 +5,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Добро пожаловать'),
+      ),
+    );
   }
 }
